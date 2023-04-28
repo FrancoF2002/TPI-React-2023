ee70f24ea29c8a6732115548f5737dc7ca2a03a1
Fausto Savoya <savoyafausto@gmail.com>
2023-04-28 12:02:16 -0300

    [ADD] Firebase config for DB, Auth & Storage, and react-router-dom library

--- TpiReact/src/firebase/credentials.js
+++ TpiReact/src/firebase/credentials.js
@@ -1,6 +1,27 @@
-require('dotenv').config();
+require("dotenv").config();
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  getBytes,
+} from "firebase/storage";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  getDoc,
+  query,
+  where,
+  setDoc,
+  deleteDoc,
+} from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,4 +36,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db  = getFirestore(app);
+const storage  = getStorage(app);
